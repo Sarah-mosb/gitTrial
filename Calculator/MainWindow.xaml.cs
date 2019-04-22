6d48edcea3e6ac4340c86a859c2f0b7b07261bb4
sarah-ahmed <sarahmosbbb@gmail.com>
2019-04-22 14:43:55 +0200

    second commit

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        int? x;
+        int? y;
+        string operand;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void GetElement(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void GetElement(object sender, MouseEventArgs e)
+        {
+            Button btn = ((Button)e.OriginalSource);
+            string val= btn.Content.ToString();
+          
+            if(val=="*"||val=="/"||val=="+"||val=="_")
+            {
+                operand = val;
+                x = int.Parse(txtBox.Text);
+                txtBox.Text = "";
+            }
+            else if(val=="=")
+            {
+                y = int.Parse(txtBox.Text);
+                if(x!= null &&  y!=null && operand != null)
+                {
+                    switch (operand)
+                    {
+                        case "*":
+                            txtBox.Text = (x * y).ToString();
+                            break;
+                        case "/":
+                            txtBox.Text = (x /y).ToString();
+                            break;
+                        case "+":
+                            txtBox.Text = (x + y).ToString();
+                            break;
+                        case "_":
+                            txtBox.Text = (x - y).ToString();
+                            break;
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("An operation is missing");
+                }
+               
+            }
+            else
+            {
+                txtBox.Text += val;
+            }
+            
+        
+        }
+
+        private void Subtract(object sender, RoutedEventArgs e)
+        {
+         
+        }
+    }
+}
